e08e6fa95c375dac3d77fbdc547028f59427570d
voonna.sriram.dev <voonna.sriram.dev@gmail.com>
2025-07-09 16:20:35 -0400

    fixing the upload error issue
    fixing the upload error issue

--- SmartDocAiApi/Controllers/UploadController.cs
+++ SmartDocAiApi/Controllers/UploadController.cs
@@ -14,14 +14,20 @@ namespace SmartDocAiApi.Controllers
         [Consumes("multipart/form-data")]
         public async Task<IActionResult> Upload([FromForm] UploadDocumentRequest request)
         {
-            if (request.File == null || request.File.Length == 0)
+            try
             {
-                return BadRequest("No file Uploaded");
+                if (request.File == null || request.File.Length == 0)
+                {
+                    return BadRequest("No file Uploaded");
+                }
+
+                var savedDocument = await _documentService.UploadDocumentsAsync(request.File);
+                return Ok(savedDocument);
             }
-
-            var savedDocument = await _documentService.UploadDocumentsAsync(request.File);
-            return Ok(savedDocument);
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal Server Error: {ex.Message}");
+            }
         }
-
     }
 }
